370d3d1f06ddc4e7037a33c1c5e4d0d45f977a62
Adam Lusted <adam.lusted@isobar.com>
2019-04-02 22:27:07 +1100

    Added image!

--- index.js
+++ index.js
@@ -2,19 +2,41 @@
 
 const program = require("commander");
 const fetch = require("node-fetch");
-const isNull = require("lodash.isnull");
+const isNull = require("lodash/isnull");
 const chalk = require("chalk");
-const pad = require("lodash.pad");
-const sample = require("lodash.sample");
+const pad = require("lodash/pad");
+const sample = require("lodash/sample");
+const flatMapDeep = require("lodash/flatMapDeep");
 
 const ABC_API_ENDPOINT =
   "https://music.abcradio.net.au/api/v1/plays/search.json?station=triplej";
 const RED = "#E03125";
 let header = " Now Playing ";
 let footer = " On Triple J ";
-const line = sample([" ", "─", "=", "-=", "░", "♥ ", "■", "■ ", "😊  "]);
 
-program.version("1.0.5").parse(process.argv);
+const lineCollection = [
+  { symbol: "■", rarity: 25 },
+  { symbol: "─", rarity: 25 },
+  { symbol: "=", rarity: 20 },
+  { symbol: "-=", rarity: 20 },
+  { symbol: "✦", rarity: 20 },
+  { symbol: "<>", rarity: 20 },
+  { symbol: "░", rarity: 15 },
+  { symbol: " ", rarity: 10 },
+  { symbol: "/\\", rarity: 10 },
+  { symbol: "↯ ", rarity: 10 },
+  { symbol: "★ ", rarity: 10 },
+  { symbol: "■ ", rarity: 5 },
+  { symbol: "∆ ", rarity: 5 },
+  { symbol: "😊  ", rarity: 3 },
+  { symbol: "♥ ", rarity: 1 }
+];
+
+const line = sample(
+  flatMapDeep(lineCollection, line => Array(line.rarity).fill(line.symbol))
+);
+
+program.version("1.0.6").parse(process.argv);
 
 const Service_PlaySearch = () => {
   return fetch(ABC_API_ENDPOINT)
